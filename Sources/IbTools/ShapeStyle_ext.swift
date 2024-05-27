16d29f13281885f119e2e411b1a7408a6d78353e
Ibrahim <ibrahimfuseini88@yahoo.com>
2024-05-27 21:29:16 +0100

    created shapestyle ext

---
+++ Sources/IbTools/ShapeStyle_ext.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ibrahim fuseini on 27/05/2024.
+//
+
+import SwiftUI
+
+public extension ShapeStyle where Self == Color {
+  static var orangePrimary: Color {
+    Color(.orangePrimary)
+  }
+}
