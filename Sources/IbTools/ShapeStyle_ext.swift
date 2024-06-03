ae41bc35a87b3eb5fa3d1f05727e54755b768a2d
Ibrahim <ibrahimfuseini88@yahoo.com>
2024-06-03 18:45:54 +0100

    added mutiple Colors and their shapestyle

--- Sources/IbTools/ShapeStyle_ext.swift
+++ Sources/IbTools/ShapeStyle_ext.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 /// This extension is in place to allow custom made colors to be used on the `ShapeStyle` protocol
 public extension ShapeStyle where Self == Color {
-  /// This is the `orangePrimary` color 
+
+  // MARK: - Primary Colors
+  /// This is the `orangePrimary` color
   static var orangePrimary: Color {
     Color(.orangePrimary)
+  }
+
+  static var bluePrimary: Color {
+    Color(.bluePrimary)
+  }
+
+  static var greenPrimary: Color {
+    Color(.greenPrimary)
+  }
+
+  static var midnightPrimary: Color {
+    Color(.midnightPrimary)
+  }
+
+  static var purplePrimary: Color {
+    Color(.purplePrimary)
+  }
+
+  static var redPrimary: Color {
+    Color(.redPrimary)
+  }
+
+  static var yellowPrimary: Color {
+    Color(.yellowPrimary)
   }
 }
