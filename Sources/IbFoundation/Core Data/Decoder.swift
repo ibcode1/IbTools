24e04d614fe9089fb0ed4b73b793b06d8ae11ff8
Ibrahim <ibrahimfuseini88@yahoo.com>
2024-08-15 21:20:52 +0100

    added logging and core data helpers

---
+++ Sources/IbFoundation/Core Data/Decoder.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ibrahim fuseini on 12/08/2024.
+//
+
+import CoreData
+
+public extension JSONDecoder {
+
+    static func jsonDecoder(
+        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
+        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+        nsManagedObjectContext: NSManagedObjectContext? = nil
+    ) -> JSONDecoder {
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+
+        if let context = nsManagedObjectContext {
+            decoder.userInfo[CodingUserInfoKey.managedObjectContext] = context
+        }
+
+        return decoder
+    }
+}
+
+extension CodingUserInfoKey {
+  static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")!
+}
+
---
+++ Sources/IbFoundation/Logging/Error.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ibrahim fuseini on 12/08/2024.
+//
+
+import Foundation
+
+enum DecodingConfigurationError: Error {
+  case missingManagedObjectContext
+}
